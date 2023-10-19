4db0fe998b93eebe98711b305d9363b331566253
Qiuyi Chen <1536196979@qq.com>
2023-10-19 15:06:46 +0800

    update

--- dataset_pubmed.py
+++ dataset_pubmed.py
@@ -122,33 +122,30 @@ class BiDataset_val(torch.utils.data.Dataset):
 
 
 class BalancedBatchSampler(Sampler):
-    def __init__(self, dataset, batch_size):
+    def __init__(self, dataset, batch_size, positive_ratio=0.5):
         self.dataset = dataset
+        self.batch_size = batch_size
+        self.positive_ratio = positive_ratio
         self.all_positive_indices = [
             i for i, label in enumerate(self.dataset.label) if label == 1
         ]
         self.all_negative_indices = [
             i for i, label in enumerate(self.dataset.label) if label == 0
         ]
-        self.batch_size = batch_size
 
     def __iter__(self):
-
-        random.shuffle(self.all_positive_indices)
-        random.shuffle(self.all_negative_indices)
         total_batches = len(self.dataset) // self.batch_size
-        num_positive_per_batch = max(1, len(self.all_positive_indices) // total_batches)
-        num_repeat = total_batches * num_positive_per_batch // len(self.all_positive_indices)
-        remaining = total_batches * num_positive_per_batch % len(self.all_positive_indices)
-        self.all_positive_indices = self.all_positive_indices * num_repeat + random.sample(self.all_positive_indices, remaining)
+        num_positive_per_batch = int(self.batch_size * self.positive_ratio)
+        num_negative_per_batch = self.batch_size - num_positive_per_batch
 
         for i in range(total_batches):
-            positive_indices = self.all_positive_indices[i * num_positive_per_batch: (i + 1) * num_positive_per_batch] 
-            negative_indices = random.sample(self.all_negative_indices, self.batch_size - len(positive_indices))
+            positive_indices = random.choices(self.all_positive_indices, k=num_positive_per_batch)          
+            negative_indices = random.choices(self.all_negative_indices, k=num_negative_per_batch)
             batch_indices = positive_indices + negative_indices
             random.shuffle(batch_indices)
+            
             for index in batch_indices:
                 yield index
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.dataset) // self.batch_size
